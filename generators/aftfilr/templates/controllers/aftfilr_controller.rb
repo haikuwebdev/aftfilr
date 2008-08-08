77d38bdbcac7b48f7892302b44fd2c61c1522f50
TJ Stankus <tj@stank.us>
2008-08-08 15:24:19 -0400

    Updated controller and model templates.

--- generators/aftfilr/templates/controllers/aftfilr_controller.rb
+++ generators/aftfilr/templates/controllers/aftfilr_controller.rb
@@ -1,3 +1,49 @@
 class <%= controller_class_name %>Controller < ApplicationController
   
+  # GET /<%= plural_name %>
+  def index
+    @<%= plural_name %> = <%= model_class_name %>.find(:all)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.js {}
+    end
+  end
+  
+  # GET /<%= plural_name %>/new
+  def new
+    @<%= singular_name %> = <%= model_class_name %>.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.js {}
+    end
+  end
+  
+  # POST /<%= plural_name %>
+  def create
+    @<%= singular_name %> = <%= model_class_name %>.new(params[:<%= singular_name %>])
+
+    respond_to do |format|
+      if @<%= singular_name %>.save
+        flash[:notice] = '<%= model_class_name %> was successfully created.'
+        format.html { redirect_to(<%= plural_name %>_url) }
+        format.js {}
+      else
+        format.html { render :action => "new" }
+        format.js {}
+      end
+    end
+  end
+  
+  # DELETE /<%= plural_name %>/1
+  def destroy
+    @<%= singular_name %> = <%= model_class_name %>.find(params[:id])
+    @<%= singular_name %>.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(<%= plural_name %>_url) }
+      format.js {}
+    end
+  end
 end
--- generators/aftfilr/templates/models/attfu_model.rb
+++ generators/aftfilr/templates/models/attfu_model.rb
@@ -1,3 +1,4 @@
 class <%= model_class_name %> < ActiveRecord::Base
-  has_attachment :storage => :file_system
+  has_attachment :storage => :file_system, :max_size => 10.megabytes
+  validates_as_attachment
 end
